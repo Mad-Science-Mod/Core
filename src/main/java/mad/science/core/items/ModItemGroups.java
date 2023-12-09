e1cc91ce26fa94241f17fcf2580c2259585e86c5
JesusChristYourLord <jules198harris@gmail.com>
2023-12-09 14:16:30 +1300

    Added Items and Itemgroups and "Food"

---
+++ src/main/java/mad/science/core/items/ModItemGroups.java
@@ -0,0 +1,24 @@
+package mad.science.core.items;
+
+import mad.science.core.MadScience;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static ItemGroup MAD_SCIENCE = Registry.register(Registries.ITEM_GROUP, new Identifier(MadScience.MODID, "mad_science"),
+            FabricItemGroup.builder().displayName(Text.translatable("Mad Science"))
+                    .icon(() -> new ItemStack(Items.IRON_BLOCK)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.FOOD);
+                        entries.add(Items.ITEM_FRAME);
+                    }).build());
+
+    public static void registerItemGroups() {
+
+    }
+}
---
+++ src/main/java/mad/science/core/items/ModItems.java
@@ -0,0 +1,21 @@
+package mad.science.core.items;
+
+import mad.science.core.MadScience;
+import net.minecraft.item.FoodComponents;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item FOOD = registerItem("food",
+            new Item((new Item.Settings().food(FoodComponents.COOKIE))));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MadScience.MODID, name), item);
+    }
+
+    public static void registerModItems() {
+        MadScience.LOGGER.info("Registering Mod Items for" + MadScience.MODID);
+    }
+}
