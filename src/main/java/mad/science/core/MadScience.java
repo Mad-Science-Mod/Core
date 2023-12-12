6aa823b8f9ae70ff3a91409198b90e2829abad6d
JesusChristYourLord <jules198harris@gmail.com>
2023-12-12 18:27:40 +1300

    added gen for some ores and lots of items with no function

--- src/main/java/mad/science/core/MadScience.java
+++ src/main/java/mad/science/core/MadScience.java
@@ -7,12 +7,23 @@ import mad.science.core.registry.ModItemGroups;
 import mad.science.core.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MadScience implements ModInitializer {
-	public static final String MODID = "mad-science";
+	public static final String MODID = "mad_science";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	public static final RegistryKey<PlacedFeature> TUBEROUS_DIRT_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MODID ,"tuberous_dirt"));
+	public static final RegistryKey<PlacedFeature> SHADOWSILK_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MODID ,"shadowsilk_ore"));
+	public static final RegistryKey<PlacedFeature> FRACTAL_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MODID ,"fractal_ore"));
 
 	@Override
 	public void onInitialize() {
@@ -24,5 +35,10 @@ public class MadScience implements ModInitializer {
 
 		// Events
 		CommonEvents.init();
+
+		// WorldGen
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, TUBEROUS_DIRT_PLACED_KEY);
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, SHADOWSILK_ORE_PLACED_KEY);
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, FRACTAL_ORE_PLACED_KEY);
 	}
 }
--- src/main/java/mad/science/core/registry/ModBlocks.java
+++ src/main/java/mad/science/core/registry/ModBlocks.java
@@ -16,7 +16,15 @@ import net.minecraft.util.math.intprovider.UniformIntProvider;
 public class ModBlocks {
 
     public static final Block TUBEROUS_DIRT = registerBlock("tuberous_dirt",
-            new Block(FabricBlockSettings.copyOf(Blocks.DIRT).strength(1.0f)
+            new Block(FabricBlockSettings.copyOf(Blocks.DIRT).strength(0.7f)
+            ));
+
+    public static final Block SHADOWSILK_ORE = registerBlock("shadowsilk_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_IRON_ORE).strength(4.5F, 3.0F).requiresTool()
+            ));
+
+    public static final Block FRACTAL_ORE = registerBlock("fractal_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).requiresTool().strength(3.0F, 3.0F)
             ));
 
     private static Block registerBlock(String name, Block block) {
